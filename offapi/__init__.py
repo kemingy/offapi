e554b525f52f584dfc100285cfc63fde4e3f5bca
Keming <kemingy94@gmail.com>
2025-08-22 11:20:38 +0800

    fix: replace resources.open_text with resources.files
    Signed-off-by: Keming <kemingy94@gmail.com>

--- offapi/__init__.py
+++ offapi/__init__.py
@@ -8,23 +8,35 @@ from offapi.templates import ReDocTemplate, ScalarTemplate, SwaggerTemplate
 __all__ = ["OpenAPITemplate"]
 
 
-with resources.open_text("offapi", "url.json") as f:
+with (resources.files("offapi") / "url.json").open(encoding="utf-8") as f:
     urls = json.load(f)
 
+
 def load_resource_file(filename: str) -> str:
-    with resources.open_text("offapi", filename) as f:
-        content = f.read()
+    content = (resources.files("offapi") / filename).read_text(encoding="utf-8")
     # escape curly braces
     return content.replace("{", "{{").replace("}", "}}")
 
 
 class OpenAPITemplate(Enum):
     SWAGGER = SwaggerTemplate.safe_substitute(
-        {PurePath(key).stem: load_resource_file(key) for key in urls if key.startswith("swagger")}
+        {
+            PurePath(key).stem: load_resource_file(key)
+            for key in urls
+            if key.startswith("swagger")
+        }
     )
     REDOC = ReDocTemplate.safe_substitute(
-        {PurePath(key).stem: load_resource_file(key) for key in urls if key.startswith("redoc")}
+        {
+            PurePath(key).stem: load_resource_file(key)
+            for key in urls
+            if key.startswith("redoc")
+        }
     )
     SCALAR = ScalarTemplate.safe_substitute(
-        {PurePath(key).stem: load_resource_file(key) for key in urls if key.startswith("scalar")}
+        {
+            PurePath(key).stem: load_resource_file(key)
+            for key in urls
+            if key.startswith("scalar")
+        }
     )
